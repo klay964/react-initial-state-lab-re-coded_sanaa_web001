6d0f9fab932abe2d519d6013f80ccd65b5e28c09
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2021-03-18 20:07:45 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
 // your Bomb code here!
+import React from "react"
+
+class Bomb extends React.Component{
+  constructor(props){
+    super()
+    this.state={
+      secondsLeft:this.props.initialCount
+    }
+  }
+  
+  render(){
+    const bombStatus=this.state.secondsLeft===0?"Boom!":`${secondsLeft}seconds left before I go boom!`
+    return (
+    
+     {bombStatus}
+      
+      )
+  }
+  
+  
+  
+}
+export default Bomb
